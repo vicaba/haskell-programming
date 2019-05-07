80a2295e4f0fcc85e3e17421c0df9a85709abebb
vicaba <vicaba@me.com>
2019-05-07 22:09:29 +0200

    concursSAlleFest

--- concursSalleFest/p2hard.hs
+++ concursSalleFest/p2hard.hs
@@ -13,15 +13,15 @@ arq = "ARQ"
 ani = "ANI"
 
 mMembers = sort [eng, ade, arq, ani]
+mMembersSet = MultiSet.fromList mMembers
 
 sol :: [String] -> Integer
 sol t = solRec (MultiSet.fromList $ sort t) 0
 
 solRec :: MultiSet.MultiSet String -> Integer -> Integer
 solRec t acc = do
-    let r = (MultiSet.fromList mMembers) `MultiSet.intersection` (t)
-    if MultiSet.size r == 4 then
-        solRec (t `MultiSet.difference` (MultiSet.fromList mMembers)) (acc + 1)
+    if MultiSet.size (mMembersSet `MultiSet.intersection` t) == 4 then
+        solRec (t `MultiSet.difference` (mMembersSet)) (acc + 1)
     else
         acc
 
@@ -32,11 +32,10 @@ funZip :: [a -> b] -> [a] -> [b]
 funZip = zipWith (\f x -> f x)
 
 main = do
- withFile "/home/fitxer_entrada_2f.txt" ReadMode (\handle -> do
+ withFile "/home/fitxer_entrada_2d.txt" ReadMode (\handle -> do
   contents <- hGetContents handle
   let linesOfFile = lines contents
-  --let groups = [ (toInt $ g !! 0, words $ g !! 1) | g <- chunksOf 2 (tail linesOfFile)]
-  --let solutions = map (\r -> if r then "POTS" else "STOP") (map sol groups)
-  --let formattedOutput = map (\(x, y) -> "Test #" ++ (show x) ++ ": " ++ (y)) (zip [1..] solutions)
-  --writeFile "/home/solp2easy.txt" $ intercalate "\n" formattedOutput
-  print "a")
+  let groups = [ words $ g !! 1 | g <- chunksOf 2 (tail linesOfFile)]
+  let solutions = map sol groups
+  let formattedOutput = map (\(x, y) -> "Test #" ++ (show x) ++ ": " ++ (show y)) (zip [1..] solutions)
+  writeFile "/home/solp2hard.txt" $ intercalate "\n" formattedOutput)
